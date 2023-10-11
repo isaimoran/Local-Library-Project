fb3e426bb20c310fa0a339506fe77b70ddd2c002
Isai Moran <135295630+isaimoran@users.noreply.github.com>
2023-10-11 14:45:31 -0700

    Update home.js
    Updated home.js code

--- public/src/home.js
+++ public/src/home.js
@@ -1,13 +1,13 @@
 function getTotalBooksCount(books) {
-  // YOUR SOLUTION HERE
+return books.length;
 }
 
 function getTotalAccountsCount(accounts) {
-  // YOUR SOLUTION HERE
+  return accounts.length;
 }
 
 function getBooksBorrowedCount(books) {
-  // YOUR SOLUTION HERE
+    return books.filter((book) => book.borrows [0].returned === false) .length;
   // Hint: You can use the [`filter()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) method here. 
   // If you get stuck, feel free to take a look at this repl.it: https://replit.com/@thinkful/getBooksBorrowedCount#index.js
 }
